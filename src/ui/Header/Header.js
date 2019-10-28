845cd35adaeb10aecc37e0f1161e9081b11c4d15
Artem Bazunts <sowasolutionslab@gmail.com>
2019-10-28 17:24:25 +0500

    change logo

--- src/ui/Header/Header.js
+++ src/ui/Header/Header.js
@@ -3,11 +3,13 @@ import {UserInfoContainer} from "../Login/";
 import {translate} from "react-i18next";
 import '../../resources/less/styles.less'
 import {compose} from "redux";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return <Fragment>
         <header className={'header'}>
             <div className="header-wrap">
+                <NavLink to={"/"} className="logo"/>
                 <div className="header-actions">
                     <div className="header__dropdown js-profile-toggler">
                         <UserInfoContainer/>
